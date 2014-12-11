5f32f67408f31bed7f63c91250df514a3b90c271
Timothy Roe, Jr. <timroejr@msn.com>
2014-12-10 21:41:53 -0800

    Update unitcircle.cc

--- unitcircle.cc
+++ unitcircle.cc
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 #include "led-matrix.h"
-
+#include "threaded-canvas-manipulator.h"
 #include <unistd.h>
 #include <math.h>
 
@@ -16,13 +16,39 @@ using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
 
-static void DrawOnCanvas(Canvas *canvas) {
+class UnitCircle : public ThreadedCanvasManipulator {
+public:
+	DrawCircle(Canvas *canvas) : ThreadedCanvasManipulator(canvas) {}
+	void Run() {
+		int center_x = canvas->width() / 2;
+		int center_y = canvas->height() / 2;
+		float radius_max = canvas->width() / 2;
+		float angle_step = 1.0 / 360;
+		for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
+			float dot_x = cos(a * 2 * M_PI) * r;
+			float dot_y = sin(a * 2 * M_PI) * r;
+			canvas->SetPixel(center_x + dot_x, center_y + dot_y, 255, 0, 0);
+			usleep(1 * 1000);  // wait a little to slow down things.
+		}
+		int width = canvas->width();
+		int height = canvas->height();
+		for (int x = 0; x < width; x++) {
+			canvas()->SetPixel(x, height/2, 0, 255, 0);
+		}
+		for (int y = 0; y < height; y++) {
+			canvas()->SetPixel(width/2, y, 0, 255, 0);
+		}
+	}	
+}
+
+
+//static void DrawOnCanvas(Canvas *canvas) {
   /*
    * Let's create a simple animation. We use the canvas to draw
    * pixels. We wait between each step to have a slower animation.
    */
   //canvas->Fill(0, 0, 255);
-
+/* 
   int center_x = canvas->width() / 2;
   int center_y = canvas->height() / 2;
   float radius_max = canvas->width() / 2;
@@ -34,9 +60,9 @@ static void DrawOnCanvas(Canvas *canvas) {
                      255, 0, 0);
     usleep(1 * 1000);  // wait a little to slow down things.
   }
-}
+} */
 
-static void DrawCenterLine(Canvas *led) {
+/* static void DrawCenterLine(Canvas *led) {
 	int width = led->width();
 	int height = led->height();
 	for (int x = 0; x < width; x++) {
@@ -46,7 +72,7 @@ static void DrawCenterLine(Canvas *led) {
 		canvas()->SetPixel(width/2, y, 0, 255, 0);
 	}
 	
-}
+} */
 
 int main(int argc, char *argv[]) {
   /*
@@ -62,9 +88,11 @@ int main(int argc, char *argv[]) {
   int rows = 32;   // A 32x32 display. Use 16 when this is a 16x32 display.
   int chain = 1;   // Number of boards chained together.
   Canvas *canvas = new RGBMatrix(&io, rows, chain);
+  ThreadedCanvasManipulator *img_gen = NULL;
+  img_gen = new UnitCircle(canvas)
 
-  DrawOnCanvas(canvas);    // Using the canvas.
-  DrawCenterLine(canvas);
+
+  
   // Animation finished. Shut down the RGB matrix.
   //canvas->Clear();
   //delete canvas;
